5dbb5e1b5e16637617bc6726f8a635b222cbecd8
Andrew Pauling <andrewpauling109@gmail.com>
2024-11-14 01:35:56 +0000

    update getdata script

--- getdata.sh
+++ getdata.sh
@@ -1,6 +1,37 @@
 #!/bin/bash
 
 workdir=$(pwd)
+
+if [[ ! -d data/processed ]]; then
+    mkdir -p data/processed
+fi
+cd data/processed
+
+wget https://zenodo.org/records/14110477/files/era5.tar.gz
+tar xzvf era5.tar.gz
+cd $workdir
+
+if [[ ! -d data/processed/HadGEM3-GC31-LL ]]; then
+    mkdir -p data/processed/HadGEM3-GC31-LL
+fi
+
+cd data/processed/HadGEM3-GC31-LL
+wget https://zenodo.org/records/14110477/files/historical.tar.gz
+wget https://zenodo.org/records/14110477/files/ssp245.tar.gz
+wget https://zenodo.org/records/14110477/files/ssp585.tar.gz
+wget https://zenodo.org/records/14110477/files/hist-antwater-92-11.tar.gz
+wget https://zenodo.org/records/14110477/files/ssp585-ismip6-water.tar.gz
+wget https://zenodo.org/records/14110477/files/fx.tar.gz
+
+tar xzvf historical.tar.gz
+tar xzvf ssp245.tar.gz
+tar xzvf ssp585.tar.gz
+tar xzvf hist-antwater-92-11.tar.gz
+tar xzvf ssp585-ismip6-water.tar.gz
+tar xzvf fx.tar.gz
+
+cd $workdir
+
 monvars=(tas tos zos uas vas pr)
 dayvars=(tasmax sfcWind)
 
