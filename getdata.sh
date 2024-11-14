35d09a9db67f345619c5383bc752883b74a10453
Andrew Pauling <andrewpauling109@gmail.com>
2024-11-14 03:02:59 +0000

    Complete getdata script

--- getdata.sh
+++ getdata.sh
@@ -7,8 +7,12 @@ if [[ ! -d data/processed ]]; then
 fi
 cd data/processed
 
-wget https://zenodo.org/records/14110477/files/era5.tar.gz
-tar xzvf era5.tar.gz
+if [[ ! -d era5 ]]; then
+    wget https://zenodo.org/records/14110477/files/era5.tar.gz
+    tar xzvf era5.tar.gz
+    rm era5.tar.gz
+fi
+
 cd $workdir
 
 if [[ ! -d data/processed/HadGEM3-GC31-LL ]]; then
@@ -16,19 +20,36 @@ if [[ ! -d data/processed/HadGEM3-GC31-LL ]]; then
 fi
 
 cd data/processed/HadGEM3-GC31-LL
-wget https://zenodo.org/records/14110477/files/historical.tar.gz
-wget https://zenodo.org/records/14110477/files/ssp245.tar.gz
-wget https://zenodo.org/records/14110477/files/ssp585.tar.gz
-wget https://zenodo.org/records/14110477/files/hist-antwater-92-11.tar.gz
-wget https://zenodo.org/records/14110477/files/ssp585-ismip6-water.tar.gz
-wget https://zenodo.org/records/14110477/files/fx.tar.gz
-
-tar xzvf historical.tar.gz
-tar xzvf ssp245.tar.gz
-tar xzvf ssp585.tar.gz
-tar xzvf hist-antwater-92-11.tar.gz
-tar xzvf ssp585-ismip6-water.tar.gz
-tar xzvf fx.tar.gz
+if [[ ! -d historical ]]; then
+    wget https://zenodo.org/records/14110477/files/historical.tar.gz
+    tar xzvf historical.tar.gz
+    rm historical.tar.gz
+fi
+if [[ ! -d ssp245 ]]; then
+    wget https://zenodo.org/records/14110477/files/ssp245.tar.gz
+    tar xzvf ssp245.tar.gz
+    rm ssp245.tar.gz
+fi
+if [[ ! -d ssp585 ]]; then
+    wget https://zenodo.org/records/14110477/files/ssp585.tar.gz
+    tar xzvf ssp585.tar.gz
+    rm ssp585.tar.gz
+fi
+if [[ ! -d hist-antwater-92-11 ]]; then
+    wget https://zenodo.org/records/14110477/files/hist-antwater-92-11.tar.gz
+    tar xzvf hist-antwater-92-11.tar.gz
+    rm hist-antwater-92-11.tar.gz
+fi
+if [[ ! -d ssp585-ismip6-water ]]; then
+    wget https://zenodo.org/records/14110477/files/ssp585-ismip6-water.tar.gz
+    tar xzvf ssp585-ismip6-water.tar.gz
+    rm ssp585-ismip6-water
+fi
+if [[ ! -d fx ]]; then
+    wget https://zenodo.org/records/14110477/files/fx.tar.gz
+    tar xzvf fx.tar.gz
+    rm fx.tar.gz
+fi
 
 cd $workdir
 
@@ -85,11 +106,15 @@ for exp in ${experiments[@]}; do
 
         cd $vdir
 
-	    . ${workdir}/nzmeltwater/wget-${var}-day-${exp}.sh -s
+        if [[ "${exp}" != "ssp585" && "${var}" != "sfcWind" ]]; then
+            . ${workdir}/nzmeltwater/wget-${var}-day-${exp}.sh -s
+        fi
 
 	    cd $workdir	
     
     done
 
 done
+
+echo "DONE"
 
